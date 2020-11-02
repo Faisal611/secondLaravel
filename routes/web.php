cd2767cc1a645e6b4921f27c094203520e543fed
Faisal611 <71620505+Faisal611@users.noreply.github.com>
2020-11-02 21:01:05 +0600

    public

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('user', 'publicController@index');
+
+Route::post('add-user', 'publicController@create');
