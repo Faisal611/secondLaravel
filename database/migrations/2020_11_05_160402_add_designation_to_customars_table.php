5952fe02d4fa3a5987e9aac373a76a13a97326f3
Faisal611 <71620505+Faisal611@users.noreply.github.com>
2020-11-08 15:54:32 +0600

    public

---
+++ database/migrations/2020_11_05_160402_add_designation_to_customars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDesignationToCustomarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customars', function (Blueprint $table) {
+            $table->string('designation')->nullable()->after('description');
+            $table->text('description')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customars', function (Blueprint $table) {
+            $table->dropColumn('designation');
+        });
+    }
+}
